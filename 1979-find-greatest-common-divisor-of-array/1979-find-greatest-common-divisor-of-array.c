c9da725ce501f05bc753901da0c74b6c1cab7ad0
Ankita Pan <panankita4@gmail.com>
2026-07-19 12:41:56 +0530

    Time: 0 ms (100%), Space: 9 MB (92.15%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.c
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.c
@@ -1,26 +1,3 @@
-// void sorting(int* arr, int n){
-//      for (int i = 0; i < n - 1; i++) {
-//         for (int j = 0; j < n - i - 1; j++) {
-//             if (arr[j] > arr[j + 1]) {
-//                 int temp = arr[j];
-//                 arr[j] = arr[j + 1];
-//                 arr[j + 1] = temp;
-//             }
-//         }
-//     }
-// }
-
-// int findGCD(int* nums, int numsSize) {
-//     sorting(nums, numsSize);
-//     int a = nums[0], b = nums[numsSize-1];
-//     while (b != 0) {
-//         int temp = b;
-//         b = a % b;
-//         a = temp;
-//     }
-//     return a;
-// }
-
 int gcd(int a, int b) {
     while (b != 0) {
         int temp = b;
